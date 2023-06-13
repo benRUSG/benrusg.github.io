fa81e1e085656a9a0559fb17595367330bae6b5c
benrusg <benjamindrouven@protonmail.com>
2023-06-13 11:23:40 +0200

    Image h2 p reveald + message sent

---
+++ js/reveal.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+   var reveals = document.querySelectorAll('#book img, #book h2, #book p, #book figcaption');
+
+   for(var i = 0; i < reveals.length; i++){
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 10;
+
+      if(revealtop < windowheight - revealpoint){
+         reveals[i].classList.add('revealed')
+      }
+      else{
+         reveals[i].classList.remove('revealed')
+      }
+   }
+}
+
+
+
