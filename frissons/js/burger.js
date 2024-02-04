fa0367abe1e0b5ef16a8209a01a2209c41ba8b55
benRUSG <benjamindrouven@pm.me>
2024-02-04 18:45:27 +0100

    frissons

--- frissons/js/burger.js
+++ frissons/js/burger.js
@@ -1,31 +1,13 @@
-window.addEventListener('scroll', burgermenu);
 
 const burger = document.querySelector('#burger');
-const burgercontainer = document.querySelector('#burger-container');
 const wrapper = document.querySelector('#menu-wrapper');
-//const burgerchecker = document.querySelector('#burger-checker');
 
-function burgermenu(){
-
-   var top = document.querySelector('body');
-   //var windowheight = window.innerHeight;
-   var revealtop = top.getBoundingClientRect().top;
-   var revealpoint = -369;
-
-   if(revealtop < revealpoint){
-      burgercontainer.classList.add('on');
-      wrapper.classList.add('slide');
-   }
-   else{
-      burgercontainer.classList.remove('on');
-      wrapper.classList.remove('slide');
-      burger.classList.remove('checked');
-      wrapper.classList.remove('in');
-   }
-
-}
-
-burger.addEventListener("click", function(){
+const toogleMenu = () => {
    burger.classList.toggle('checked');
    wrapper.classList.toggle('in');
- });
+ }
+
+burger.addEventListener('click', () => {
+   toogleMenu();
+});
+
