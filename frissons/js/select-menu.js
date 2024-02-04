edd6f98fccbd6027524d1c2761e2cc228fd2b086
benRUSG <benjamindrouven@pm.me>
2024-02-04 12:10:29 +0100

    frissons

--- frissons/js/select-menu.js
+++ frissons/js/select-menu.js
@@ -1,4 +1,4 @@
-const select = document.querySelector('.select');
+/* const select = document.querySelector('.select');
 const optionBox = document.querySelector('.options');
 const options = [...document.querySelectorAll('.options .item')];
 
@@ -6,6 +6,57 @@ let activeOption = 0; // default should be 0
 
 window.onclick = (e) => {
     if(!e.target.className.is('select')){
+        select.classList.remove('active');
+        optionBox.classList.remove('active');
+    } else{
+        select.classList.toggle('active');
+        optionBox.classList.toggle('active');
+    }
+}
+
+options.forEach((item, i) => {
+    item.onmousemove = () => {
+        hoverOptions(i);
+    }
+})
+
+const hoverOptions = (i) => {
+    options[activeOption].classList.remove('active');
+    options[i].classList.add('active');
+    activeOption = i;
+    setValue();
+}
+
+window.onkeydown = (e) => {
+    if(select.className.includes('active')){
+        e.preventDefault();
+        if(e.key === 'ArrowDown' && activeOption < options.length - 1){
+            hoverOptions(activeOption + 1);
+        } else if(e.key === 'ArrowUp' && activeOption > 0){
+            hoverOptions(activeOption - 1);
+        } else if(e.key === 'Enter'){
+            select.classList.remove('active');
+            optionBox.classList.remove('active');
+        }
+    }
+}
+
+const setValue = () => {
+    select.innerHTML = select.value = options[activeOption].innerHTML;
+}
+
+setValue(); */
+
+
+
+const select = document.querySelector('.select');
+const optionBox = document.querySelector('.options');
+const options = [...document.querySelectorAll('.options .item')];
+
+let activeOption = 0; // default should be 0
+
+window.onclick = (e) => {
+    if(!e.target.classList.contains('select')){
         select.classList.remove('active');
         optionBox.classList.remove('active');
     } else{
