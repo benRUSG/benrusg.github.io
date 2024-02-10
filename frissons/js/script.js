f16b73647ef7256abf95aef0e821e8b8dce52b49
benRUSG <benjamindrouven@pm.me>
2024-02-10 14:35:04 +0100

    Carousel etc

--- frissons/js/script.js
+++ frissons/js/script.js
@@ -25,13 +25,41 @@ function checkHeaderHeight() {
     const headerHeight = styles.height;
     // set CSS as a value
     let elementList = document.querySelectorAll('.hero-grid');
+    let elementListChild = document.querySelectorAll('.hero-img');
 
     for (let element of elementList) {
       element.style.setProperty("height", 'calc( 100svh - ' + headerHeight + ')');
+    }
+    for (let element of elementListChild) {
+      element.style.setProperty("height", 'calc( 100svh - ' + headerHeight + ')');
+      element.style.setProperty("width", 'calc( 100svh - ' + headerHeight + ')');
     }
   }
   addEventListener("resize", checkHeaderHeight);
   addEventListener("orientationchange", checkHeaderHeight);
   checkHeaderHeight();
 
-  
+
+  // Tab Outside btn
+
+const activePane = document.querySelector('.tab-pane.active');
+const activeTab = document.querySelector('.nav-link.active');
+const trigger = document.querySelectorAll('.tab-btn');
+
+for(var i =0; i < trigger.length; i++) {
+  (function(i) {
+    trigger[i].onmousedown = function() { 
+      const tab = this.dataset['tabtarget'];
+      outsideToogleMenu(this.dataset['tabtarget'], this.dataset['panetarget']);
+    };   
+   })(i);
+}
+
+
+const outsideToogleMenu = (tab, pane) => {
+   activeTab.classList.toggle('active');
+   activePane.classList.remove('active', 'show');
+
+   document.querySelector(tab).classList.add('active');
+   document.querySelector(pane).classList.add('active', 'show');
+ }
