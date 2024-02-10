13575e838ebec6b3e515d1f89e01a35db7efe4ad
benRUSG <benjamindrouven@pm.me>
2024-02-10 15:25:10 +0100

    suite

--- frissons/js/script.js
+++ frissons/js/script.js
@@ -1,5 +1,5 @@
 // Carousel
-const myCarouselElement = document.querySelector('#carouselExampleRide')
+const myCarouselElement = document.querySelector('#carousel-hero-section')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 6000
@@ -42,16 +42,34 @@ function checkHeaderHeight() {
 
   // Tab Outside btn
 
+ 
+/* const carousel = document.querySelector('#carousel-hero'); */
 const activePane = document.querySelector('.tab-pane.active');
 const activeTab = document.querySelector('.nav-link.active');
 const trigger = document.querySelectorAll('.tab-btn');
 
 for(var i =0; i < trigger.length; i++) {
   (function(i) {
+    trigger[i].onmouseenter = function() { 
+      myCarouselElement.setAttribute('data-bs-pause', 'hover');
+    };     
+   })(i);
+}
+
+for(var i =0; i < trigger.length; i++) {
+  (function(i) {
+    trigger[i].onmouseleave = function() { 
+      myCarouselElement.setAttribute('data-bs-pause', 'false');
+    };     
+   })(i);
+}
+
+for(var i =0; i < trigger.length; i++) {
+  (function(i) {
     trigger[i].onmousedown = function() { 
       const tab = this.dataset['tabtarget'];
       outsideToogleMenu(this.dataset['tabtarget'], this.dataset['panetarget']);
-    };   
+    };     
    })(i);
 }
 
